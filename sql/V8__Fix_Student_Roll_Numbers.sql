3d27dffd3b6ea6e7228522be01e35e51f510a217
Khurram Shahzad <khurram.1981@gmail.com>
2026-05-05 23:09:30 +0500

    Include class ID in student roll numbers (S-YY-CLASS-ID)

--- sql/V8__Fix_Student_Roll_Numbers.sql
+++ sql/V8__Fix_Student_Roll_Numbers.sql
@@ -1,3 +1,3 @@
--- V8: Standardize student roll numbers to S-YY-ID format
+-- V8: Standardize student roll numbers to S-YY-CLASS-ID format
 UPDATE students 
-SET roll_number = CONCAT('S-', DATE_FORMAT(CURDATE(), '%y'), '-', 1000 + id);
+SET roll_number = CONCAT('S-', DATE_FORMAT(CURDATE(), '%y'), '-', class_id, '-', 1000 + id);
