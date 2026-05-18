030006ae12f843f98f98b252b0975fcd0548dc4e
Khurram Shahzad <khurram.1981@gmail.com>
2026-05-18 14:26:34 +0500

    fix: ensure CR Khoram password is set to 1234 on deployment

--- update_mudeer_pass.js
+++ update_mudeer_pass.js
@@ -6,6 +6,11 @@ async function fix() {
     const hashed = await bcrypt.hash('1981', 10);
     await db.execute('UPDATE users SET password = ? WHERE role = "مدير"', [hashed]);
     console.log('Mudeer password updated to 1981');
+
+    const hashedCr = await bcrypt.hash('1234', 10);
+    await db.execute('UPDATE users SET password = ? WHERE username = "خرم"', [hashedCr]);
+    console.log('CR Khoram password updated to 1234');
+
     process.exit(0);
 }
 fix();
